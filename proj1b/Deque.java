bd1af5cb19a856b11e7c8225b9a0e26ef5045629
Hanwen Xiu <xiuhanwen@gmail.com>
2022-05-10 10:34:09 +0800

    project1b finished

---
+++ proj1b/Deque.java
@@ -0,0 +1,18 @@
+public interface Deque <T>{
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public void addFirst(T item);
+
+    public void addLast(T item);
+
+    public T removeFirst();
+
+    public T removeLast();
+
+    public T get(int index);
+
+    public void printDeque();
+}
